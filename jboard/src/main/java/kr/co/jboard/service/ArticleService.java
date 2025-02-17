631ad91b33d108e3b7d4c3c4a85967588ddc6403
sjo112777 <sjo112777@gmail.com>
2025-02-17 13:33:27 +0900

    add jboard

---
+++ jboard/src/main/java/kr/co/jboard/service/ArticleService.java
@@ -0,0 +1,32 @@
+package kr.co.jboard.service;
+
+import java.util.List;
+
+import kr.co.jboard.dao.ArticleDAO;
+import kr.co.jboard.dto.ArticleDTO;
+
+public enum ArticleService {
+	
+	INSTANCE;	
+	private ArticleDAO dao = ArticleDAO.getInstance();
+	
+	public void registeArticle(ArticleDTO dto) {
+		dao.insertArticle(dto);
+	}
+	
+	public ArticleDTO findArticle(int no) {
+		return dao.selectArticle(no);
+	}
+	
+	public List<ArticleDTO> findAllArticle() {
+		return dao.selectAllArticle();
+	}
+	
+	public void modifyArticle(ArticleDTO dto) {
+		dao.updateArticle(dto);
+	}
+	
+	public void deleteArticle(int no) {
+		dao.deleteArticle(no);
+	}
+}
