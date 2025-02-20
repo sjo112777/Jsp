a759c40bd292e7f8e8d580fcd66a08d7a87dc3fe
sjo112777 <sjo112777@gmail.com>
2025-02-20 15:04:43 +0900

    add jboard

--- jboard/src/main/java/kr/co/jboard/dao/ArticleDAO.java
+++ jboard/src/main/java/kr/co/jboard/dao/ArticleDAO.java
@@ -2,8 +2,12 @@ package kr.co.jboard.dao;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import kr.co.jboard.dto.ArticleDTO;
 import kr.co.jboard.util.DBHelper;
+import kr.co.jboard.util.SQL;
 
 public class ArticleDAO extends DBHelper {	
 	private static final ArticleDAO INSTANCE = new ArticleDAO();
@@ -11,8 +15,22 @@ public class ArticleDAO extends DBHelper {
 		return INSTANCE;
 	}
 	private ArticleDAO() {}
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	public void insertArticle(ArticleDTO dto) {
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.INSERT_ARTICLE);
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getContent());
+			psmt.setString(3, dto.getWriter());
+			psmt.setString(4, dto.getRegip());
+			psmt.executeUpdate();
+			closeAll();
+		}catch (Exception e) {
+			logger.error(e.getMessage());
+		}
 		
 	}
 	
--- jboard/src/main/java/kr/co/jboard/util/SQL.java
+++ jboard/src/main/java/kr/co/jboard/util/SQL.java
@@ -26,4 +26,13 @@ public class SQL {
 											+ "`regip`=?,"
 											+ "`regDate`=NOW()";
 
+	// article
+	public static final String INSERT_ARTICLE = "insert into `article` set "
+													+ "`title`=?,"
+													+ "`content`=?,"
+													+ "`writer`=?,"
+													+ "`regip`=?,"
+													+ "`wdate`=NOW()";
+	
+	
 }
